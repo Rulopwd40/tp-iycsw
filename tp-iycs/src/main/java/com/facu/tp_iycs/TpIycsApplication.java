c2579f6330b35326de83f7fcd52126913d359b97
Rulopwd40 <132414402+Rulopwd40@users.noreply.github.com>
2026-09-22 18:58:09 -0300

    Project init

---
+++ tp-iycs/src/main/java/com/facu/tp_iycs/TpIycsApplication.java
@@ -0,0 +1,13 @@
+package com.facu.tp_iycs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIycsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIycsApplication.class, args);
+	}
+
+}
